d9c56c9a47a4ec4b29c2164563bd8e73ff07622e
AKHIL K KULKARNI <97751536+akhilkk0803@users.noreply.github.com>
2025-02-16 16:18:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   console.log("Jenkins seekho vro");
-  res.json({ message: "Jenkins seekho vro" });
+  res.json({ message: "Jenkins seekho bro" });
 });
 console.log("HELLO");
 app.listen(8000, () => console.log("Server running on port 8000"));
